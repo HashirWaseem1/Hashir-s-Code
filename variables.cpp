cd8e369cb5f960430a9956b16c51622a164f1bc0
Hashir Waseem <hudairjano1@gmail.com>
2024-07-20 16:15:11 +0500

    First three Variables Types in cpp | Hashir's Code
    This is First three Types of variables in cpp.

---
+++ variables.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //variables Types
+    // 1. integer(int)
+    int a= 4;
+    int b= 5;
+    int c= 19;
+    cout<<"hello this is the value of a "<< a;
+    cout<<"\nThe value of b is "<< b;
+    cout<<"\nThe value of c is "<< c;
+    // 2. Float(float)
+    float pi= 3.142857143;
+    cout<<"\n The float value is "<< pi;
+    // 3. Character(char)
+    char d='i';
+    cout<<"\n The character value is "<< d;
+    //
+    return 0;
+}
